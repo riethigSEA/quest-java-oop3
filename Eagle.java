67d164b4079316d948f6bf9b923303fbd2fca0ad
riethigSEA <marco-riethig_student2021@wilder.school>
2021-09-23 15:51:53 +0200

    created Fly fixed Eagle and Nature

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -12,10 +12,34 @@ public class Eagle extends Bird {
     public int getAltitude() {
         return altitude;
     }
-
+    public void setAltitude(int meters){this.altitude = meters;}
+    public void setFlying(boolean flightStatus){ this.flying = flightStatus;}
     public boolean isFlying() {
         return flying;
     }
+
+    public void takeOff(){System.out.println(this.getName() + " is taking off!");}
+    public void land(){System.out.println(this.getName() + " is landing!"); this.setAltitude(0); this.currentheight(); this.setFlying(false);}
+    public void currentheight(){System.out.println("current height "+ this.getAltitude());}
+    public int ascend(int meters){
+        if(!isFlying()) {
+            this.setAltitude(this.getAltitude() + meters);
+
+        }
+        this.currentheight();
+        return this.altitude;
+    }
+    public int descend(int meters){
+        if(isFlying() && this.getAltitude() != 0){
+
+            this.altitude = this.getAltitude() - meters;
+
+        }
+        this.currentheight();
+        return this.altitude;
+
+    }
+
 
     @Override
     public String sing() {
